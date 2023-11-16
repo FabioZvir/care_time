ec5add39f5b3318d758dda58ea018deed0d8dbf9
Fabio Ricardo Zvir Junior <81338145+FabioZvir@users.noreply.github.com>
2023-11-16 15:19:27 -0300

    updt: inserido o dropdown

--- lib/communs/input_form/input_form.dart
+++ lib/communs/input_form/input_form.dart
@@ -2,42 +2,51 @@ import 'package:flutter/material.dart';
 
 class InputForm extends StatelessWidget {
   final String? text;
-  const InputForm({Key? key, required this.text}) : super(key: key);
+  final TextInputType type;
+  const InputForm({
+    Key? key,
+    required this.text,
+    required this.type,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Form(
       child: TextFormField(
+        keyboardType: type,
         decoration: InputDecoration(
-            enabledBorder: const OutlineInputBorder(
-              borderRadius: BorderRadius.all(
-                Radius.circular(10),
-              ),
-              borderSide: BorderSide(
-                color: Color.fromARGB(116, 0, 0, 1),
-                width: 2,
-              ),
+          enabledBorder: const OutlineInputBorder(
+            borderRadius: BorderRadius.all(
+              Radius.circular(10),
             ),
-            disabledBorder: const OutlineInputBorder(
-              borderRadius: BorderRadius.all(
-                Radius.circular(12),
-              ),
-              borderSide: BorderSide(
-                color: Color.fromARGB(116, 0, 0, 1),
-                width: 2,
-              ),
+            borderSide: BorderSide(
+              color: Color.fromARGB(116, 0, 0, 1),
+              width: 2,
             ),
-            focusedBorder: const OutlineInputBorder(
-              borderRadius: BorderRadius.all(
-                Radius.circular(12),
-              ),
-              borderSide: BorderSide(
-                color: Color.fromARGB(193, 0, 0, 1),
-                width: 2,
-              ),
+          ),
+          disabledBorder: const OutlineInputBorder(
+            borderRadius: BorderRadius.all(
+              Radius.circular(12),
             ),
-            hintText: text,
-            hintStyle: const TextStyle(color: Color.fromRGBO(0, 0, 0, 0.5))),
+            borderSide: BorderSide(
+              color: Color.fromARGB(116, 0, 0, 1),
+              width: 2,
+            ),
+          ),
+          focusedBorder: const OutlineInputBorder(
+            borderRadius: BorderRadius.all(
+              Radius.circular(12),
+            ),
+            borderSide: BorderSide(
+              color: Color.fromARGB(193, 0, 0, 1),
+              width: 2,
+            ),
+          ),
+          hintText: text,
+          hintStyle: const TextStyle(
+            color: Color.fromRGBO(0, 0, 0, 0.5),
+          ),
+        ),
       ),
     );
   }
--- lib/pages/cadastro_page/cadastro_page.dart
+++ lib/pages/cadastro_page/cadastro_page.dart
@@ -1,6 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/communs/input_form/input_form.dart';
 
+const List<String> list = <String>[
+  'Gênero',
+  'Masculino',
+  'Feminino',
+  'Prefiro não dizer'
+];
+
 class CadastrOpage extends StatefulWidget {
   const CadastrOpage({Key? key}) : super(key: key);
 
@@ -9,42 +16,60 @@ class CadastrOpage extends StatefulWidget {
 }
 
 class _CadastrOpageState extends State<CadastrOpage> {
+  String generoValor = list.first;
   @override
   Widget build(BuildContext context) {
     return Builder(builder: (context) {
       return Column(
-        children: const <Widget>[
-          Padding(
+        children: <Widget>[
+          const Padding(
             padding: EdgeInsets.fromLTRB(40, 80, 40, 10),
             child: InputForm(
               text: "Nome",
+              type: TextInputType.name,
             ),
           ),
-          Padding(
+          const Padding(
               padding: EdgeInsets.symmetric(vertical: 10, horizontal: 40),
               child: InputForm(
                 text: "Sobrenome",
+                type: TextInputType.name,
               )),
-          Padding(
+          const Padding(
               padding: EdgeInsets.symmetric(vertical: 10, horizontal: 40),
               child: InputForm(
                 text: "Idade",
+                type: TextInputType.number,
               )),
-          Padding(
+          const Padding(
               padding: EdgeInsets.symmetric(vertical: 10, horizontal: 40),
               child: InputForm(
                 text: "Peso",
+                type: TextInputType.number,
               )),
-          Padding(
+          const Padding(
               padding: EdgeInsets.symmetric(vertical: 10, horizontal: 40),
               child: InputForm(
                 text: "Altura",
+                type: TextInputType.number,
               )),
           Padding(
-              padding: EdgeInsets.symmetric(vertical: 10, horizontal: 40),
-              child: InputForm(
-                text: "Gênero",
-              )),
+            padding: EdgeInsets.symmetric(vertical: 10, horizontal: 40),
+            child: DropdownButton<String>(
+              value: generoValor,
+              onChanged: (String? value) {
+                setState(() {
+                  generoValor = value!;
+                });
+              },
+              items: list.map<DropdownMenuItem<String>>((String value) {
+                return DropdownMenuItem<String>(
+                  value: value,
+                  child: Text(value),
+                );
+              }).toList(),
+            ),
+          )
         ],
       );
     });
